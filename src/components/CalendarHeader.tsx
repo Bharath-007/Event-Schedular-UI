a71e5b15d2b770cf56d918c76ebb129fba154ef1
Bharath <bharathofficial05@gmail.com>
2025-03-20 23:07:11 +0530

    current date fixed

--- src/components/CalendarHeader.tsx
+++ src/components/CalendarHeader.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { CalendarViewProps } from "./types/types";
-import { IconButton, Tab, Tabs } from "@mui/material";
+import { IconButton, Paper, Tab, Tabs } from "@mui/material";
 
 interface CalendarHeaderProps
   extends Pick<CalendarViewProps, "view" | "onViewChange"> {
@@ -19,15 +19,16 @@ const CalendarHeader: React.FC<CalendarHeaderProps> = ({
   onNext,
   onToday,
 }) => {
-
   const getHeaderTitle = () => {
     switch (view) {
       case "day":
-        return currentDate.toLocaleDateString("en-GB", {
-          day: "numeric",
-          month: "long",
-          year: "numeric",
-        }).replace(',', '');
+        return currentDate
+          .toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })
+          .replace(",", "");
       case "week":
         const startOfWeek = new Date(currentDate);
         startOfWeek.setDate(currentDate.getDate() - currentDate.getDay());
@@ -53,23 +54,31 @@ const CalendarHeader: React.FC<CalendarHeaderProps> = ({
 
   return (
     <div className="flex justify-between items-center px-4 py-2 bg-white rounded-t-lg">
-      <div className="flex items-center space-x-1">
+      <div className="flex gap-2">
         <IconButton
           sx={{ border: "2px solid #3b82f6", borderRadius: 1, px: 1 }}
           onClick={onPrevious}
-          className="p-2 hover:bg-gray-200 transition duration-200 w-1/3 h-8"
+          className="p-0 hover:bg-gray-200 transition duration-200 w-1/3 h-8"
         >
           <FaChevronLeft />
         </IconButton>
         <IconButton
           sx={{ border: "2px solid #3b82f6", borderRadius: 1, px: 1 }}
           onClick={onNext}
-          className="p-2 hover:bg-gray-200 transition duration-200 w-1/3 h-8"
+          className="p-0 hover:bg-gray-200 transition duration-200 w-1/3 h-8"
         >
           <FaChevronRight />
         </IconButton>
+        <Paper
+          elevation={1}
+          className="font-semibold py-1 px-4 text-sky-500"
+          sx={{ visibility: view === "month" ? "hidden" : "visible" }}
+        >
+          {view === "day"
+            ? currentDate.getDate()
+            : (currentDate.getDate() - currentDate.getDay()).toLocaleString()}
+        </Paper>
       </div>
-
       <div className="flex items-center">
         <h2 className="text-xl font-semibold">{getHeaderTitle()}</h2>
       </div>
@@ -79,11 +88,22 @@ const CalendarHeader: React.FC<CalendarHeaderProps> = ({
           onChange={(_, newValue) => onViewChange(newValue)}
           variant="scrollable"
           scrollButtons="auto"
-
         >
-          <Tab label="Day" value="day" sx={{ textTransform: 'capitalize', fontWeight: 600 }} />
-          <Tab label="Week" value="week" sx={{ textTransform: 'capitalize', fontWeight: 600 }} />
-          <Tab label="Month" value="month" sx={{ textTransform: 'capitalize', fontWeight: 600 }} />
+          <Tab
+            label="Day"
+            value="day"
+            sx={{ textTransform: "capitalize", fontWeight: 600 }}
+          />
+          <Tab
+            label="Week"
+            value="week"
+            sx={{ textTransform: "capitalize", fontWeight: 600 }}
+          />
+          <Tab
+            label="Month"
+            value="month"
+            sx={{ textTransform: "capitalize", fontWeight: 600 }}
+          />
         </Tabs>
       </div>
     </div>
